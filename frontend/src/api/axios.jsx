b554b2a765e0f442d8f424992063a18d417b4605
ravimahto69 <krravi073@gmail.com>
2026-06-06 18:16:56 +0530

    improve the security

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,24 +1,18 @@
 import axios from 'axios';
 
 const API = axios.create({
-        baseURL: 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
-// Attach token from localStorage to every request if present
 API.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
+
         if (token) {
             config.headers = config.headers || {};
             config.headers.Authorization = `Bearer ${token}`;
         }
-        // Debug: log token presence and outgoing headers
-        try {
-            // eslint-disable-next-line no-console
-            console.debug('API request', config.method, config.url, 'tokenPresent=', !!token, 'headers=', config.headers);
-        } catch (e) {
-            // ignore logging errors
-        }
+
         return config;
     },
     (error) => Promise.reject(error)
