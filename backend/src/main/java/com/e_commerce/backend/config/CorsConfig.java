0d6aea95f3d652b8199f752e6c78059f92cb97c1
ravimahto69 <krravi073@gmail.com>
2026-06-06 18:22:24 +0530

    improve the cors policy

--- backend/src/main/java/com/e_commerce/backend/config/CorsConfig.java
+++ backend/src/main/java/com/e_commerce/backend/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("https://e-commerce-project-woad-three.vercel.app/")
         );
 
         configuration.setAllowedMethods(
