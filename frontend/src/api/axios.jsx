0eead6dbd2cb9dc9d107fe1a34ab0d346271f141
ravimahto69 <krravi073@gmail.com>
2026-06-06 19:04:06 +0530

    ..

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,21 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+
+console.log("Backend URL:", import.meta.env.VITE_BACKEND_URL);
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 API.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+  (config) => {
+    const token = localStorage.getItem("token");
 
-        if (token) {
-            config.headers = config.headers || {};
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-        return config;
-    },
-    (error) => Promise.reject(error)
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default API;
